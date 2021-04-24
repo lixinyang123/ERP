ab1231c73719fec73f861dfa35012a521c7116bb
lixinyang123 <lixinyangemil@outlook.com>
2021-04-25 02:32:50 +0800

    完善代码

--- server/src/main.py
+++ server/src/main.py
@@ -1,9 +1,6 @@
 from flask import Flask
-import os, uuid, json
-from datetime import datetime
-from server.src.service.SaleService import *
-from server.src.service.ProductService import *
-from server.src.service.UserService import *
+# import os, json
+# from server.src.service.SaleService import *
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -14,10 +11,12 @@ app.secret_key = os.urandom(24)
 @app.route('/')
 def hello_world():
 
-    saleService = SaleService()
-    order = saleService.find("7d3de239-4810-4a2c-81d0-a76d6960c127")
-    saleService.dispose()
+    # saleService = SaleService()
+    # order = saleService.find("7d3de239-4810-4a2c-81d0-a76d6960c127")
+    # saleService.dispose()
 
-    jsonStr = json.dumps(order.dicted())
+    # jsonStr = json.dumps(order.dicted())
 
-    return (jsonStr, 200, [("content-type", "application/json")])
+    # return (jsonStr, 200, [("content-type", "application/json")])
+
+    return "Hello World"
