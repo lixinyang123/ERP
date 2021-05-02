33fb95d2220dc8584a81ae91aa3b8b911594b327
lixinyang123 <lixinyangemil@outlook.com>
2021-05-02 16:59:22 +0800

    完善界面

---
+++ web/js/index.js
@@ -0,0 +1,62 @@
+
+async function navigation(name,isBack = false){
+
+    window.scrollTo(0,0);
+    loadingState(true);
+    document.querySelector("#main").innerHTML = "";
+    let url = "/view/" + name + ".html";
+
+    let response = await fetch(url);
+
+    if(response.status == 200) {
+        let data = await response.text()
+        document.querySelector("#main").innerHTML = data;
+        inject();
+        url = "#" + name;
+        if(!isBack)
+            history.pushState({page: name}, name, url);
+        loadingState(false);
+    }
+    else{
+        navigation("notfound");
+        loadingState(false);
+    }
+}
+
+function loadingState(flag){
+    let loadingPage = document.querySelector("#loading");
+    if(flag){
+        loadingPage.removeAttribute("hidden");
+    }
+    else{
+        loadingPage.setAttribute("hidden","hidden");
+    }
+}
+
+function inject(){
+    document.querySelectorAll("#main > script").forEach(element => {
+        let src = element.getAttribute("src");
+        request(src,(data) => {});
+    });
+}
+
+async function init(){
+
+    //初始化导航
+    window.onpopstate = (e)=>{
+        navigation(e.state.page, true);
+    }
+
+    let href = window.location.href;
+
+    //导航到指定页面
+    if(href.includes("#")){
+        let page = href.substring(href.indexOf("#")+1);
+        navigation(page);
+    }
+    else{
+        navigation("home");
+    }
+}
+
+init();
