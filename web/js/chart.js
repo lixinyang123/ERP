a19ee9e53ac04ae53b824b70a7b0f2ac2e75a304
lixinyang123 <lixinyangemil@outlook.com>
2021-05-25 03:39:56 +0800

    完善图表页面

--- web/js/chart.js
+++ web/js/chart.js
@@ -1,28 +1,153 @@
-var labels = [
-    '一月',
-    '二月',
-    '三月',
-    '四月',
-    '五月',
-    '六月',
-];
+function randomColor() {
+    let colors = ["red", "yellow", "blue", "green", "pink", "orange", "grey", "purple", "orangered"];
+    let index = Math.round(Math.random() * (colors.length - 1));
+    return colors[index];
+}
 
-var data = {
-    labels: labels,
-    datasets: [{
-        label: '销售图表',
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: [0, 10, 5, 2, 20, 30, 45],
-    }]
-};
+async function showCharts() {
+    let sales = (await (await fetch(api + "/sale/index?page=1")).json()).sales;
+    let purchases = (await (await fetch(api + "/purchase/index?page=1")).json()).purchases;
+    showProduct(sales);
+    showMoney(sales, purchases);
+}
 
-var config = {
-    type: 'line',
-    data,
-    options: {}
-};
+function getData(sales, purchases) {
+    let labels = [];
+    sales.forEach(sale => {
+        let time = sale.time.substring(0, 10);
+        if(labels.indexOf(time) < 0)
+            labels.push(time);
+    });
 
-document.querySelectorAll(".myChart").forEach(element => {
-    new Chart(element, config);
-});
+    purchases.forEach(purchase => {
+        let time = purchase.time.substring(0, 10);
+        if(labels.indexOf(time) < 0)
+            labels.push(time);
+    });
+    return labels.reverse();
+}
+
+function getPayment(purchaseOperations) {
+    let payment = 0;
+    purchaseOperations.forEach(operation => {
+        payment += operation.product.price * operation.num;
+    });
+    return payment;
+}
+
+function getPurchaseData(purchases, labels) {
+    purchases = purchases.reverse();
+    let datas = [];
+    for(let i=0; i<labels.length; i++)
+        datas.push(0);
+    
+    for(let i=0; i<purchases.length; i++) {
+        let purchase = purchases[i];
+        let time = purchase.time.substring(0, 10);
+
+        if(!purchase.state)
+            continue;
+
+        let index = labels.indexOf(time);
+        if(index >= 0)
+            datas[index] += getPayment(purchase.purchaseOperations);
+    }
+
+    return {
+        label: '支出',
+        backgroundColor: "green",
+        borderColor: "green",
+        data: datas,
+    };
+}
+
+function getAmounted(checkOuts) {
+    let amounted = 0;
+    checkOuts.forEach(checkOut => {
+        amounted += checkOut.amount;
+    });
+    return amounted;
+}
+
+function getSaleData(sales, labels) {
+    sales = sales.reverse();
+    let datas = [];
+    for(let i=0; i<labels.length; i++)
+        datas.push(0);
+
+    for(let i=0; i<sales.length; i++) {
+        let sale = sales[i];
+        let time = sale.time.substring(0, 10);
+        
+        let index = labels.indexOf(time);
+        if(index >= 0)
+            datas[index] += getAmounted(sale.checkOuts);
+    }
+
+    return {
+        label: '销售',
+        backgroundColor: "red",
+        borderColor: "red",
+        data: datas,
+    };
+}
+
+function showMoney(sales, purchases) {
+
+    let dates = getData(sales, purchases);
+
+    let config = {
+        type: 'line',
+        data: {
+            labels: dates,
+            datasets: [
+                getPurchaseData(purchases, dates),
+                getSaleData(sales, dates)
+            ]
+        }
+    };
+
+    new Chart(document.querySelector("#money"), config);
+}
+
+function showProduct(sales) {
+
+    let labels = [];
+    let datas = [];
+    let backgroundColors = [];
+
+    sales.forEach(sale => {
+        
+        for (let i = 0; i < sale.saleOperations.length; i++) {
+            let operation = sale.saleOperations[i];
+            let index = labels.indexOf(operation.product.name);
+
+            if(index < 0) {
+                labels.push(operation.product.name);
+                datas.push(operation.num);
+                backgroundColors.push(randomColor());
+            }
+            else {
+                datas[index] += operation.num;
+            }
+        }
+
+    });
+
+    let config = {
+        type: 'doughnut',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: '产品销售状况',
+                data: datas,
+                backgroundColor: backgroundColors,
+                hoverOffset: 4
+            }]
+        },
+    };
+
+    new Chart(document.querySelector("#product"), config);
+}
+
+showCharts();
